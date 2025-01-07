4924f1db9b9192459781171a99048c6d532efaf6
Gareth Whitley <me@garethwhitley.online>
2025-01-07 15:14:56 +0000

    feat: add JavaScript to hide nav menu when link is clicked or when web page is clicked and menu is open

---
+++ assets/js/navscripts.js
@@ -0,0 +1,19 @@
+const navMenu = document.getElementById("menu"); // Get nav menu
+const links = navMenu.getElementsByTagName('a'); // Get links by selected anchor tag within navMenu
+const navToggle = document.getElementById('nav-toggle') // Get navToggle check box
+const container = document.getElementById('container'); // Get container div
+
+// Loop through links and hide nav menu if checkbox is true
+for(let link of links) {
+    link.addEventListener('click', () => {
+        navToggle.checked = false;
+    })
+}
+
+// Hide nav menu if user clicks on the web page when menu is open
+container.addEventListener('click', () => {
+    if (navToggle.checked === true) navToggle.checked = false;
+});
+
+
+
